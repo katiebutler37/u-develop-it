69e6abc306a0db74e61053b43ec76d7f2b677c0b
Katie <katiebutler37@gmail.com>
2022-07-23 22:56:42 -0400

    joined tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+-- this order is significant as well
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+-- must declare tables that are referenced (their primary et for another table's foreign key) first, be mindful of table order
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -8,5 +13,7 @@ CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
